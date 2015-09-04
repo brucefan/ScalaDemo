b46abb5a199d106b49df2b99804a6cc9116f81b8
bruce01.fan <bruce01.fan@vipshop.com>
2015-09-04 16:03:45 +0800

    init

---
+++ src/scala/highorder/HighOrderDemo.scala
@@ -0,0 +1,15 @@
+package scala.highorder
+
+/**
+  * Created by bruce01.fan on 2015/9/4.
+  */
+object HighOrderDemo {
+
+
+   def main(args:Array[String]): Unit ={
+       val list = List("frodo","samwise","pippin")
+       // ¸ß½×º¯Êý
+       list.foreach(hobbit => println(hobbit))
+   }
+
+ }
